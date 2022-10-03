dc2294409e37baffa275aa4f5e44d2461f623628
Max Cunningham <maxzinedine@gmail.com>
2022-10-03 17:55:11 +0100

    First iteration validation - passing some tests

--- validation.py
+++ validation.py
@@ -1,3 +1,37 @@
 
-def validateExpression(expression: str)-> bool:
+def validateExpression(expression: str):
+    lastUnitIsOP = True
+    for i in range(len(expression)):
+        if isOperation(expression[i]):
+            if lastUnitIsOP:
+                return False, i
+            lastUnitIsOP = True
+        if isNumber(expression[i]):
+            if not lastUnitIsOP:
+                return False, i
+            lastUnitIsOP = False
+            i = findEndOfNumber(expression, i)
+
+    return not lastUnitIsOP, len(expression)
+
+
+operations = {'+', '-', '/', '*', '^'}
+def isOperation(char : str) -> bool:
+    for op in operations:
+        if char == op:
+            return True
     return False
+
+
+def isNumber(char : str) -> bool:
+    if 48 <= ord(char) <= 57:
+        return True
+    return False
+
+
+def findEndOfNumber(exp: str, curI:int) -> int:
+    for i in range(curI,len(exp)):
+        if not isNumber(exp[i]):
+            return i - 1
+    return len(exp)-1
+
--- validation_test.py
+++ validation_test.py
@@ -2,17 +2,17 @@ from validation import validateExpression
 from testing_kit import Test
 
 def test_validateExpression() -> None:
-    tests = [Test("Simple Exp No Space","1+1",True),
-             Test("Simple Exp w/ Space", "1 + 1",True),
-             Test("Brackets","(1 + 1) + 2",True),
-             Test("Multiple Operations", "1 + 2 * 3 / 7^4 - 9", True),
-             Test("Double Digits", "10 + 54",True),
-             Test("Multi Digits", "1 * 21 - 190/8 + 346759", True),
-             Test("More Numbers"," 1 1 + 1", False),
-             Test("More Operations", "1 + + 2",False),
-             Test("Incorrect Order", "1 + + 1 2", False),
-             Test("Incorrect Order 2", " 1 5 *",False),
-             Test("Incorrect Order 3", "7 - 1 / - 4 0", False)]
+    tests = [Test("Simple Exp No Space","1+1",(True, 3)),
+             Test("Simple Exp w/ Space", "1 + 1",(True, 5)),
+             Test("Brackets","(1 + 1) + 2",(True, 11)),
+             Test("Multiple Operations", "1 + 2 * 3 / 7^4 - 9", (True, 19)),
+             Test("Double Digits", "10 + 54",(True,7)),
+             Test("Multi Digits", "1 * 21 - 190/8 + 346759", (True, 23)),
+             Test("More Numbers","1 1 + 1", (False,2)),
+             Test("More Operations", "1 + + 2",(False,4)),
+             Test("Incorrect Order", "1 + + 1 2", (False,4)),
+             Test("Incorrect Order 2", " 1 5 *",(False,2)),
+             Test("Incorrect Order 3", "7 - 1 / - 4 0", (False,8))]
 
     hasFailure = False
     for t in tests:
