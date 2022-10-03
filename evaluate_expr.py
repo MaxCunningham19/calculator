5c227ec488d981ce2f1c6bc833aaa042e101a798
taylorj8 <taylorj8@tcd.ie>
2022-10-03 15:28:30 +0100

    added evaluation function

--- evaluate_expr.py
+++ evaluate_expr.py
@@ -1,17 +1,3 @@
-# isValidInput
-# @param: input_to_calc -> the input string of the user
-# @return: bool -> returns true if the string only contains valid characters
-validChars = {'+', '-', '/', '*', '^', '(', ')', ' '}
-
-
-def isValidChars(input_to_calc: str) -> bool:
-    for tmpChar in input_to_calc:
-        special = validChars.intersection({tmpChar})
-        if len(special) == 0 and (ord(tmpChar) < 48 or ord(tmpChar) > 57):
-            return False
-    return True
-
-
 def isNumber(char):
     if 48 <= ord(char) <= 57:
         return True
--- input_organiser_test.py
+++ input_organiser_test.py
@@ -1,29 +1,32 @@
 from input_organiser import isValidChars
 
-class test:
-    def __init__(self,name:str,input,output ) -> None:
+
+class Test:
+    def __init__(self, name: str, input, output) -> None:
         self.name = name
         self.input = input
         self.output = output
 
+
 def isValidCharsTest() -> None:
-    tests = [test("addition","1+1",True),
-            test("multiplication","1x2",True),
-            test("subtraction","1-1",True),
-            test("division","1%1",True),
-            test("all numbers","1+2+3+4+5+6+7+8+9+0",True),
-            test("bad input correct chars","6+",True),
-            test("brackets","(1+1)+1",True),
-            test("letters","a+b",False),
-            test("weird chars","$#^*.,",False)]
-    
+    tests = [test("addition", "1+1", True),
+             test("multiplication", "1*2", True),
+             test("subtraction", "1-1", True),
+             test("division", "1/1", True),
+             test("all numbers", "1+2+3+4+5+6+7+8+9+0", True),
+             test("bad input correct chars", "6+", True),
+             test("brackets", "(1+1)+1", True),
+             test("letters", "a+b", False),
+             test("weird chars", "$#^*.,", False)]
+
     fails = []
     for t in tests:
         result = isValidChars(t.input)
-        if  t.output != result:
+        if t.output != result:
             fails.append(t)
-            print("Test",t.name,"Fails Expected:",t.output,"Got:",result)
+            print("Test", t.name, "Fails Expected:", t.output, "Got:", result)
     print("isValidCharTest Complete...")
-    assert(len(fails) == 0)
+    assert (len(fails) == 0)
+
 
 isValidCharsTest()
