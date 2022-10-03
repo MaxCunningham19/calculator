219c6bca9a5c7066d260af54749511f86d0d583b
taylorj8 <taylorj8@tcd.ie>
2022-10-03 15:58:13 +0100

    fixed bugs and handled unary operators

--- input_organiser.py
+++ input_organiser.py
@@ -21,33 +21,36 @@ def isNumber(char):
 def convertToList(input_to_calc: str):
     # no_space = input_to_calc.replace(" ", "")
     last_num = False
+    next_unary = True  # if true, next operator will be applied to number (for negation)
+    negate = False
     expr = []
     for char in input_to_calc:
         if isNumber(char):
             if last_num:
-                expr[-1] = expr[-1] * 10 + (ord(char)-48)    # convert string to number, add onto end of number in list
+                if negate:
+                    expr[-1] = expr[-1] * 10 - (ord(char) - 48)  # convert string to number, subtract from end of number in list
+                else:
+                    expr[-1] = expr[-1] * 10 + (ord(char)-48)    # convert string to number, add onto end of number in list
             else:
                 last_num = True
-                expr.append(ord(char)-48)     # convert from string to number and add to list
+                if negate:
+                    expr.append((ord(char)-48)*-1)
+                else:
+                    expr.append(ord(char)-48)     # convert from string to number and add to list
+            next_unary = False  # if operator follows number, it is not unary
+            negate = False
         elif char == ' ':
             last_num = False
-        else:
+        elif char == '-' and next_unary:
+            negate = True
+        elif char == ')':
             last_num = False
             expr.append(char)
+        else:
+            last_num = False
+            next_unary = True
+            expr.append(char)
     return expr
-
-
-def applyOp(val1, op, val2):
-    if op == "+":
-        return val1 + val2
-    elif op == "-":
-        return val1 - val2
-    elif op == "*":
-        return val1 * val2
-    elif op == "/":
-        return val1 / val2
-    elif op == "^":
-        return val1 ^ val2      # shouldn't be any other possible values as validChar function handles those cases
 
 
 def getPrecedence(token):
@@ -63,6 +66,19 @@ def getPrecedence(token):
         return 3        # shouldn't be any other possible values as validChar function handles those cases
 
 
+def applyOp(val1, op, val2):
+    if op == "+":
+        return val1 + val2
+    elif op == "-":
+        return val1 - val2
+    elif op == "*":
+        return val1 * val2
+    elif op == "/":
+        return val1 / val2
+    elif op == "^":
+        return val1 ^ val2      # shouldn't be any other possible values as validChar function handles those cases
+
+
 def act(val_stack, op_stack):
     op = op_stack.pop()
     val2 = val_stack.pop()
@@ -71,7 +87,7 @@ def act(val_stack, op_stack):
     val_stack.append(res)
 
 
-def evaluatePostfix(expr):
+def evaluate(expr):
     val_stack = []
     op_stack = []
     for token in expr:
@@ -84,7 +100,7 @@ def evaluatePostfix(expr):
                 act(val_stack, op_stack)
             op_stack.pop()  # discard "("
         else:
-            while len(op_stack) != 0 and getPrecedence(op_stack[-1]) >= getPrecedence(token):
+            while len(op_stack) != 0 and op_stack[-1] != '(' and getPrecedence(op_stack[-1]) >= getPrecedence(token):
                 act(val_stack, op_stack)
             op_stack.append(token)
 
@@ -99,7 +115,7 @@ if __name__ == "__main__":
     stuff_to_calculate = input("Enter your calculation: ")
     if isValidChars(stuff_to_calculate):
         test = convertToList(stuff_to_calculate)
-        result = evaluatePostfix(test)
+        result = evaluate(test)
         print(result)
     else:
         print("error")
