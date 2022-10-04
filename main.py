602112d6325b0dd1f17b488680d1b1ad8f2b5efe
Max Cunningham <maxzinedine@gmail.com>
2022-10-04 14:43:50 +0100

    Added validation to main

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from input_organiser import calculate
+from validation import validateExpression
 
 if __name__ == "__main__":
     print("Welcome to simple calculator")
@@ -8,6 +9,10 @@ if __name__ == "__main__":
         if stuff_to_calculate == "quit":
             running = False
         else:
-            result = calculate(stuff_to_calculate)
-            print(result)
+            isValid, index = validateExpression(stuff_to_calculate)
+            if isValid:
+                result = calculate(stuff_to_calculate)
+                print(result)
+            else:
+                print("Invalid Input found at position",index)
     print("Program terminated")
