e728f74d4f11d6ba8e06a2d038f3e2af7e1ffae2
taylorj8 <taylorj8@tcd.ie>
2022-10-05 16:10:45 +0100

    started function for more detailed error messages

--- validation.py
+++ validation.py
@@ -1,21 +1,49 @@
 from util import isNumber, isOperation
 
 
-def validateExpression(expression: str):
-    last_unit_is_op = True
-    i = 0
-    while i < len(expression):
-        if isOperation(expression[i]):
-            if last_unit_is_op and expression[i] != '-':
-                return False, i
-            last_unit_is_op = True
-        elif isNumber(expression[i]):
-            if not last_unit_is_op:
-                return False, i
-            last_unit_is_op = False
-            i = findEndOfNumber(expression, i)
-        i = i + 1
-    return not last_unit_is_op, len(expression)
+# def validateExpression(expression: str):
+#     last_unit_is_op = True
+#     i = 0
+#     while i < len(expression):
+#         if isOperation(expression[i]):
+#             if last_unit_is_op and expression[i] != '-':
+#                 return False, i
+#             last_unit_is_op = True
+#         elif isNumber(expression[i]):
+#             if not last_unit_is_op:
+#                 return False, i
+#             last_unit_is_op = False
+#             i = findEndOfNumber(expression, i)
+#         i = i + 1
+#     return not last_unit_is_op, len(expression)
+
+
+def validateExpression(expression: []):
+    if isOperation(expression[0]):
+        return "Error: starts with operator"
+
+    last_op = ''
+    brackets = 0
+    for i in expression:
+        if type(i) == int:
+            last_op = ''
+        elif isOperation(i):
+            if last_op != '':
+                return "Error: two operators in a row: " + last_op + " and " + i
+            last_op = i
+        elif i == '(':
+            brackets += 1
+        elif i == ')':
+            brackets -= 1
+        else:
+            return "Error: unrecognised character: " + i
+
+    if brackets > 0:
+        return "Error: open left bracket"
+    if brackets < 0:
+        return "Error: open right bracket"
+
+    return None
 
 
 validChars = {'+', '-', '/', '*', '^', '(', ')', ' '}
