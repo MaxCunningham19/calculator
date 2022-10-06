6dd4a44c09abab42d3d26b058d84e0d4b820450c
Max Cunningham <maxzinedine@gmail.com>
2022-10-06 16:51:29 +0100

    linter validation.pyt

--- validation.py
+++ validation.py
@@ -12,41 +12,41 @@ def validateExpression(expression):
     if isOperator(expression[-1]):
         return "Error: ends with operator"
 
-    last_op = ' '
-    last_num = ''
-    last_bracket = ''
+    last_op = " "
+    last_num = ""
+    last_bracket = ""
     brackets = 0
     for i in expression:
         if type(i) == int:
-            if last_num != '':
+            if last_num != "":
                 return "Error: two numbers in a row: " + str(last_num) + " and " + str(i)
-            if last_bracket == ')':
+            if last_bracket == ")":
                 return "Error: operator needed after right bracket"
-            last_op = ''
+            last_op = ""
             last_num = i
-            last_bracket = ''
+            last_bracket = ""
         elif isOperator(i):
-            if last_op != '':
+            if last_op != "":
                 return "Error: two operators in a row: " + last_op + " and " + i
-            if last_bracket == '(':
+            if last_bracket == "(":
                 return "Error: operator after left bracket"
             last_op = i
-            last_num = ''
-            last_bracket = ''
-        elif i == '(':
-            if last_op == '':
+            last_num = ""
+            last_bracket = ""
+        elif i == "(":
+            if last_op == "":
                 return "Error: operator needed before left bracket"
             brackets += 1
-            last_op = ''
-            last_num = ''
-            last_bracket = '('
-        elif i == ')':
-            if last_op != '':
+            last_op = ""
+            last_num = ""
+            last_bracket = "("
+        elif i == ")":
+            if last_op != "":
                 return "Error: operator before right bracket"
             brackets -= 1
-            last_op = ''
-            last_num = ''
-            last_bracket = ')'
+            last_op = ""
+            last_num = ""
+            last_bracket = ")"
         else:
             return "Error: unrecognised character: " + i
 
