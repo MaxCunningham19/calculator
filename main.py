52353f3dd6e93b9860b83d9c49f84ce06cbd2367
Max Cunningham <maxzinedine@gmail.com>
2022-10-07 13:40:08 +0100

    refactored main & eval to reduce seams.
    eval was calling util and validation which are now called in main.
    This gives greater testability and main as the main poc for the entire program, as well as the user.

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 from evaluation import calculate
+import util as util
+import validation as valid
+
+
+def processInput(stuff_to_calculate):
+    if stuff_to_calculate == "quit":
+        return False
+    list_to_calculate = util.convertToList(stuff_to_calculate)
+    validation_result = valid.validateExpression(list_to_calculate)
+    if validation_result is not None:
+        print(validation_result)
+    else:
+        result = calculate(stuff_to_calculate)
+        print(result)
+    return True
+
 
 if __name__ == "__main__":
     print("Welcome to simple calculator")
     running = True
     while running:
         stuff_to_calculate = input("Enter your calculation or quit: ")
-        if stuff_to_calculate == "quit":
-            running = False
-        else:
-            result = calculate(stuff_to_calculate)
-            print(result)
+        running = processInput(stuff_to_calculate)
     print("Program terminated")
