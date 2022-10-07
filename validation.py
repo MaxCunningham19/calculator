db12ed3c6393b83bef9a7d38c60d934df81fac93
Max Cunningham <maxzinedine@gmail.com>
2022-10-07 14:11:32 +0100

    formatting

--- validation.py
+++ validation.py
@@ -1,5 +1,4 @@
 from typing import Any, List
-
 from util import isOperator
 
 
@@ -24,7 +23,8 @@ def validateExpression(expression: List[Any]):
         if type(i) == int:
             if last_num != "":
                 return (
-                    "Error: two numbers in a row: " + str(last_num) + " and " + str(i)
+                    "Error: two numbers in a row: " +
+                    str(last_num) + " and " + str(i)
                 )
             if last_bracket == ")":
                 return "Error: operator needed after right bracket"
