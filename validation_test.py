b868b4678e4ae3c71afd586cbaca65f2d3531767
taylorj8 <taylorj8@tcd.ie>
2022-10-07 12:13:36 +0100

    more formatting changes

--- validation_test.py
+++ validation_test.py
@@ -1,6 +1,6 @@
-from validation import validateExpression
 from testing_kit import Test
 
+from validation import validateExpression
 
 def test_validateExpression() -> None:
     tests = [
@@ -9,12 +9,12 @@ def test_validateExpression() -> None:
         Test("Brackets", ["(", 1, "+", 1, ")", "+", 2], None),
         Test("Multi Ops", [1, "+", 2, "*", 3, "/", 7, "-", 9], None),
         Test(
-            "Multi Digits", 
+            "Multi Digits",
             [1, "*", 21, "-", 190, "/", 8, "+", 346759],
             None,
         ),
         Test(
-            "More Numbers", 
+            "More Numbers",
             [1, 1, "+", 1],
             "Error: two numbers in a row: 1 and 1",
         ),
@@ -26,7 +26,7 @@ def test_validateExpression() -> None:
         Test("Operator first", ["*", 45], "Error: starts with operator"),
         Test("Operator last", [45, "/"], "Error: ends with operator"),
         Test(
-            "Left bracket error", 
+            "Left bracket error",
             ["(", 56, "+", 45],
             "Error: open left bracket",
         ),
